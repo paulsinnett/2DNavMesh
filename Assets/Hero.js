39c11c4cff97f34bc1830e5c329c228e220aa53c
Paul Sinnett <paul.sinnett@gmail.com>
2015-03-11 14:14:07 +0000

    Sample using NavMesh for pathfinding in 2D
    Initial commit

---
+++ Assets/Hero.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+private var navigation: NavMeshAgent;
+var goal: Transform;
+
+function Start () {
+
+	navigation = GetComponent.<NavMeshAgent>();
+	navigation.SetDestination(goal.position);
+}
